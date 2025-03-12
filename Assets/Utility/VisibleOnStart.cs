851a758a48fca3bf297e498f24da24c7f5eaad64
Tommé Joshua Nöll <tnGit@my.mail.de>
2025-03-12 22:37:53 +0100

    build win + webgl

--- Assets/Utility/VisibleOnStart.cs
+++ Assets/Utility/VisibleOnStart.cs
@@ -6,6 +6,8 @@ public class VisibleOnStart : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        #if UNITY_EDITOR
         SceneVisibilityManager.instance.Hide(gameObject, false);
+        #endif
     }
 }
