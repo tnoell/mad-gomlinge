299bcf39b7d2749192e47df2475d68d0de764011
Tommé Joshua Nöll <tnGit@my.mail.de>
2025-03-13 14:51:37 +0100

    main menu partial

--- Assets/Ui/MenuActions.cs
+++ Assets/Ui/MenuActions.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+public class MenuActions : MonoBehaviour
 {
     // [SerializeField] private string sceneName;
 
     public void Load(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
