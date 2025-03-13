50584a65e4164d5660c3135f0bc600e4f267b23a
Tommé Joshua Nöll <tnGit@my.mail.de>
2025-03-13 20:43:01 +0100

    sound when bullet hits truck

--- Assets/Combat/Attack.cs
+++ Assets/Combat/Attack.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Combat
 {
     public class Attack : MonoBehaviour
     {
+        public UnityEvent onHit;
         [SerializeField] private float damageAmount;
 
         public void Hit(DamageSource source, Combatant target)
         {
+            onHit.Invoke();
             target.Hit(damageAmount, source);
         }
 
--- Assets/Utility/Detacher.cs
+++ Assets/Utility/Detacher.cs
@@ -4,9 +4,21 @@ public class Detacher : MonoBehaviour
 {
     [SerializeField] private Transform obj;
     [SerializeField] private Transform makeSiblingOf;
+    [SerializeField] private bool activate = true;
+    [SerializeField] private float destroyAfter = -1;
 
     public void Detach()
     {
-        obj.parent = makeSiblingOf?.parent;
+        Transform parent = null;
+        if(makeSiblingOf) parent = makeSiblingOf.parent;
+        obj.parent = parent;
+        if(activate)
+        {
+            obj.gameObject.SetActive(true);
+        }
+        if(destroyAfter >= 0)
+        {
+            GameObject.Destroy(obj.gameObject, destroyAfter);
+        }
     }
 }
