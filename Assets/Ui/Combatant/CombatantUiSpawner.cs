86f4f6111db0a82d30b04cbb0af8dc7e08f0aa42
Tommé Joshua Nöll <tnGit@my.mail.de>
2025-03-13 12:14:48 +0100

    player health bar

---
+++ Assets/Ui/Combatant/CombatantUiSpawner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Ui
+{
+    public class CombatantUiSpawner : MonoBehaviour
+    {
+        void Start()
+        {
+            // UiManager.GetInstance().AddCombatantUi(GetComponent<Combat.Combatant>());
+        }
+    }
+}
