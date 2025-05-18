d9c1020f234c04eb8a1186a4605bac293f9e37f9
Tommé Joshua Nöll <tnGit@my.mail.de>
2025-05-18 10:24:39 +0200

    Machinegun minigame: start with some bullets already placed

--- Assets/Maintenance/Minigames/Machinegun/MachinegunMinigame.cs
+++ Assets/Maintenance/Minigames/Machinegun/MachinegunMinigame.cs
@@ -1,8 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MachinegunMinigame : Minigame
 {
-    [SerializeField] private GameObject[] bullets;
+    [SerializeField] private List<GameObject> bullets;
+    [SerializeField] private List<GameObject> bulletSlots;
+    [SerializeField] private int initiallyCompleted;
+
+    void Start()
+    {
+        for(int i = 0; i < initiallyCompleted; i++)
+        {
+            int iBullet = Random.Range(0, bullets.Count);
+            GameObject.Destroy(bullets[iBullet]);
+            bullets.RemoveAt(iBullet);
+
+            int iSlot = Random.Range(0, bulletSlots.Count);
+            bulletSlots[iSlot].GetComponent<DragReceiver>().onReceive.Invoke();
+            bulletSlots.RemoveAt(iSlot);
+        }
+    }
 
     void FixedUpdate()
     {
