1568ee4010306f9546eabe9e16ebe9a29e403ebd
Tommé Joshua Nöll <tnGit@my.mail.de>
2025-06-09 17:27:01 +0200

    don't cache children's icon sequence

--- Assets/Encounters/Icons/IconSequence.cs
+++ Assets/Encounters/Icons/IconSequence.cs
@@ -7,14 +7,17 @@ public class IconSequence : MonoBehaviour
 
     public List<Sprite> Get()
     {
-        if (icons.Count == 0)
+        if (icons.Count > 0) return icons;
+
+        List<Sprite> iconsFromChildren = new List<Sprite>(); // We don't cache this, because
+            // this is likely to be called on a prefab, so the data might stick around after
+            // the prefab gets edited. It wouldn't be a huge problem, but the performance cost
+            // of not caching it also shouldn't be significant.
+        foreach (Transform child in transform)
         {
-            foreach (Transform child in transform)
-            {
-                IconSequence childIcons = child.GetComponent<IconSequence>();
-                if (childIcons) icons.AddRange(childIcons.Get());
-            }
+            IconSequence childIcons = child.GetComponent<IconSequence>();
+            if (childIcons) iconsFromChildren.AddRange(childIcons.Get());
         }
-        return icons;
+        return iconsFromChildren;
     }
 }
